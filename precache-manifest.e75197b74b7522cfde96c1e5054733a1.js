950ed9c58479500c884d0bef35fe67274a3dc46c
Alex Al-Hamdan <alex@arccentric.com>
2020-07-17 08:36:26 -0500

    Updates

---
+++ precache-manifest.e75197b74b7522cfde96c1e5054733a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de389a10cf795aeda84ebdf969c64b7",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "f6630404d8202e4e9854",
+    "url": "/memory-game/static/css/main.1c5d4125.chunk.css"
+  },
+  {
+    "revision": "5e03821771a2908474dd",
+    "url": "/memory-game/static/js/2.e1963c71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.e1963c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6630404d8202e4e9854",
+    "url": "/memory-game/static/js/main.f9a966ca.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
